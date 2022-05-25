c594b199db59ffc6e5d81add6fd187e3a0dc6710
Nikoloz Tvildiani <ww.bingonemo@gmail.com>
2022-05-25 22:03:33 +0400

    Sync

--- dep/gui.py
+++ dep/gui.py
@@ -4,8 +4,9 @@ class Vis():
     def __init__(self, lu):
         self.lu = lu
 
-    def loop_flow(self):
+    def loop_flow(self, lu):
         while True:
+            self.lu = lu
             print(self.lu.x)
             time.sleep(1)
             
--- main.py
+++ main.py
@@ -3,11 +3,10 @@ from dep.worker import Worker
 from dep.gui import Vis
 
 w = Worker()
-v = Vis()
 
 if __name__ == '__main__':
     p1 = mp.Process(target=w.run)
-    p2 = mp.Process(target=v.loop_flow)
+    p2 = mp.Process(target=w.gui.loop_flow, args=(w,))
     p1.start()
     p2.start()
     p1.join()
